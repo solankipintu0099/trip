cfc47674559e17c5791a59f053fce5fb7d3ee824
solankipintu0099 <109578179+solankipintu0099@users.noreply.github.com>
2024-04-30 12:06:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import required modules
+const express = require('express');
+
+// Create an instance of Express application
+const app = express();
+
+// Define a route handler for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, Trix!');
+});
+
+const PORT = process.env.PORT || 4000;
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
